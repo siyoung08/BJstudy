07348d8aa263e9c86ec1a7c7db225e07a73e7aaf
Siyoung Choi <Siyoung Choi@192.168.25.57>
2021-02-17 14:34:49 +0900

    Second commit!

--- src/num/num1.java
+++ src/num/num1.java
@@ -5,18 +5,17 @@ public class num1{
 	public static void main(String args[]){
 		Scanner in =new Scanner(System.in);
 		
-		int a,b,c;
 		
-		a = in.nextInt();
-		b = in.nextInt();
-		c = in.nextInt();
+		
+		int a = in.nextInt();
+		String b = in.next();
 		
 		in.close();
 		
-		System.out.println((a+b)%c);
-		System.out.println(((a%c) + (b%c))%c );
-		System.out.println((a*b)%c);
-		System.out.println(((a % c) * (b%c))%c);
+		System.out.println(a * (b.charAt(2) - '0'));
+		System.out.println(a * (b.charAt(1) - '0'));
+		System.out.println(a *(b.charAt(0) - '0'));
+		System.out.println(a * Integer.parseInt(b));
 	}
 }
 
---
+++ src/num/num2588.java
@@ -0,0 +1,17 @@
+package num;
+
+import java.util.*;
+public class num2588{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.println(a + b);
+	}
+}
+
+

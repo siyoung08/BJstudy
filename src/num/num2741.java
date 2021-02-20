c25eebdcd07a765d5c0c3fdc67fbf7f720343d96
Siyoung Choi <Siyoung Choi@192.168.25.57>
2021-02-20 12:05:53 +0900

    !

---
+++ src/num/num2741.java
@@ -0,0 +1,19 @@
+package num;
+
+import java.util.Scanner;
+public class num2741{
+	public static void main(String args[]){
+		Scanner sc =new Scanner(System.in);
+		
+	
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i=1; i<=n; i++){
+			System.out.println(i);
+		}
+		
+}
+}
+
+

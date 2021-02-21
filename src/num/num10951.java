6918b256e32daf40b2ce0656433dcdc0ffbc4961
Siyoung Choi <Siyoung Choi@192.168.25.57>
2021-02-21 21:31:49 +0900

    !

---
+++ src/num/num10951.java
@@ -0,0 +1,21 @@
+package num;
+
+import java.util.Scanner;
+
+public class num10951 {
+ 
+    public static void main(String args[]) {
+        Scanner in= new Scanner(System.in);
+        
+        while(in.hasNextInt()){
+        	
+        
+        int A = in.nextInt();
+        int B = in.nextInt();
+        System.out.println(A+B);
+       }
+       in.close();
+    }
+}
+
+

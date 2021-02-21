3c6152c92bc6e28416215064d1b66c8742edfc65
Siyoung Choi <Siyoung Choi@192.168.25.57>
2021-02-21 19:27:20 +0900

    ~

---
+++ src/num/num2439.java
@@ -0,0 +1,25 @@
+package num;
+
+import java.util.Scanner;
+
+public class num2439{
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+         
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n-i; ++j) {
+                System.out.print(" ");
+            }
+            for(int t=1; t<=i; ++t) {
+            System.out.println("*");
+        }
+            System.out.println();
+    }
+  }
+}
+
+
+

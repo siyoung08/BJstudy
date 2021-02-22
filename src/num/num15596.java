4877403164b434b98ad38437aa688faa9384f972
Siyoung Choi <Siyoung Choi@192.168.25.57>
2021-02-22 21:19:27 +0900

    !@

---
+++ src/num/num15596.java
@@ -0,0 +1,13 @@
+package num;
+
+public class num15596 {
+    long sum (int[] a) {
+        long ans = 0;
+        for (int i=0; i <a.length; i ++) {
+       
+    }
+        return ans;
+  }
+}
+
+

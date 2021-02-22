c8687c4e67eee51f493f3382703cef7be27c4ba6
Siyoung Choi <Siyoung Choi@192.168.25.57>
2021-02-22 11:52:37 +0900

    ~

---
+++ src/num/num3052.java
@@ -0,0 +1,21 @@
+package num;
+
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class num3052{
+ 
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+       HashSet <Integer> h = new HashSet<Integer>();
+    		   
+        for(int i = 0; i<10; i++) {
+        	h.add(in.nextInt() %42);
+        }
+        in.close();
+        System.out.println(h.size());
+      }
+    }
+
+
+

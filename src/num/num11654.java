4eb6b6129ef2dc2a204ccb1a61107f280e6b2dbb
Siyoung Choi <Siyoung Choi@192.168.25.40>
2021-02-23 11:12:06 +0900

    ^^

---
+++ src/num/num11654.java
@@ -0,0 +1,14 @@
+package num;
+
+import java.util.Scanner;
+
+public class num11654 {
+ 
+    public static void main(String args[]) {
+        Scanner in= new Scanner(System.in);
+        
+      int ch = in.next().charAt(0);
+      System.out.println(ch);
+    }
+}
+

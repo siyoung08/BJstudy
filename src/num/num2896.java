daa8ab924767c505090778dbc793be0a984ea49a
Siyoung Choi <happycloudy0308@gmail.com>
2021-02-25 11:54:24 +0900

    !

---
+++ src/num/num2896.java
@@ -0,0 +1,32 @@
+package num;
+
+import java.util.Scanner;
+
+public class num2896 {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int up = in.nextInt();		// A
+		int down = in.nextInt();	// B
+		int length = in.nextInt(); 	// C
+ 
+		int day = (length - down) / (up - down);
+        
+		// 나머지가 있을 경우 (잔여 블럭이 있을 경우)
+		if ((length - down) % (up - down) != 0) {
+			day++;
+		}
+		System.out.println(day);
+		in.close();
+	}
+	
+}
+
+
+
+		
+	  
+	
+

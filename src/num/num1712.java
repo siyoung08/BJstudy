581e81016b42f0644400edcc9561dd4e53a9d55d
Siyoung Choi <happycloudy0308@gmail.com>
2021-02-25 10:27:31 +0900

    %

---
+++ src/num/num1712.java
@@ -0,0 +1,31 @@
+package num;
+
+import java.util.Scanner;
+
+public class num1712 {
+	
+	static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+        int A =in.nextInt();
+        int B =in.nextInt();
+        int C =in.nextInt();
+        
+        if(C<=B) {
+        	System.out.println("-1");
+        }
+        else {
+        	System.out.println((A/(C-B))+1);
+        }
+
+	}
+	}
+
+	
+
+
+		
+	  
+	
+

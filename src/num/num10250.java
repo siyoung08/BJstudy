8598b75639c2be73c91a80eca69c0a3e823edf53
Siyoung Choi <happycloudy0308@gmail.com>
2021-02-25 12:26:57 +0900

    A

--- src/num/num10250.java
+++ src/num/num10250.java
@@ -6,13 +6,13 @@ public class num10250 {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
- 
+ in.close();
 		int T =in.nextInt();
 		
 		for(int i =0; i<T; i++) {
 			
 			int H =in.nextInt();
-			int W =in.nextInt();
+			//int W =in.nextInt();
 			int N =in.nextInt();
 			
 			if (N%H ==0) {
